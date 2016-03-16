927b0f2e9905b40f6e19422b42912137fbfb8fc1
Sergii <Sergii_Kudriashov@epam.com>
2016-03-16 05:52:10 +0200

    test server

---
+++ server.js
@@ -0,0 +1,34 @@
+"use strict"
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const PORT = 3030;
+const handler = (req, res) => {
+  let reqUrl = req.url;
+  let method = req.method.toLowerCase();
+  if (url === '/favicon.ico') {
+    res.writeHead(200, {
+      'Content-Type': 'image/x-icon'
+    });
+    res.end();
+    return;
+  }
+
+  if (reqUrl == '/' && method == 'get') {
+    fs.readFile('./views/home.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+  }
+  if (reqUrl == '/not' && method == 'get') {
+    fs.readFile('./views/not.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+  }
+}
+
+http.createServer(handler).listen(PORT);
